b2a15dec05d12c83ed32d676dbb90f4dcb9a6ae4
z58occc <a0981583030@gmail.com>
2024-11-02 02:31:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
 
   const [arr, setArr] = useState([]);
 
-  const num = [...Array(12)].map((_, i) => i + 13);
-  const insideNum = [...Array(12)].map((_, i) => i + 1);
+  const num = [...Array(12)].map((_, i) => i + 13);//產生13~24的陣列
+  
+  const indicator = [...Array(72)].map((_, i) => i + 1);//產生1~72的陣列
+
+  const insideNum = [...Array(12)].map((_, i) => i + 1);//產生1~12的陣列
 
 
 
@@ -48,34 +51,78 @@ function App() {
           <div className='clock-face d-flex'>
             {num.map((n, i) => {
               return (
-                <span className='clock-number' key={i}
+                <div key={i}
                   style={{
-                    transform: `rotate(${360 / 12 * (i + 10)}deg) translate(150px) rotate(-${360 / 12 * (i + 10)}deg)`
+                    position:'absolute',
+                    color:'white',
+                    fontSize:'12px',
+                    transform: `rotate(${360 / 12 * (i + 10)}deg) translate(140px) rotate(-${360 / 12 * (i + 10)}deg)`
+                    // 第一個參數相對於圓心轉幾度 第二參數平移（半徑）第三個參數 修正數字本身的角度（轉回來）
                   }}
                 >
                   {n}
-                </span>
+                </div>
               )
             })}
+            {indicator.map((n, i) => {
+
+              if (i % 3 == 0 && i % 6 != 0) {//被3整除不被6整除return 星星
+                return (
+                    <div className='star '
+                    style={{
+                      position:'absolute',
+                      transform: `rotate(${(360 / 72) * i}deg) translate(120px) `
+                    }}
+                    ></div>
+                )
+              } else if (i % 6 == 0) {//被6整除return 直線
+                return (
+                  
+                    <div className='line '
+                    style={{
+                      width: "24px",
+                      transform: `rotate(${(360 / 72) * i}deg) translate(120px) `
+
+                    }}
+                    ></div>
+                )
+
+              }
+              else {//剩下的return 白點
+                return (
+                    <div className='border'
+                    style={{
+                      position:'absolute',
+                      transform: `rotate(${(360 / 72) * i}deg) translate(120px) `
+                    }}
+                    ></div>
+                )
+              }
+            }
+
+            )}
             {insideNum.map((n, i) => {
               return (
-                <span className='clock-number' key={i}
+                <div  key={i}
                   style={{
-                    transform: `rotate(${360 / 12 * (i + 10)}deg) translate(100px) rotate(-${360 / 12 * (i + 10)}deg)`
+                    position:'absolute',
+                    color:'white',
+                    fontSize:'12px',
+                    transform: `rotate(${360 / 12 * (i + 10)}deg) translate(103px) rotate(-${360 / 12 * (i + 10)}deg)`
                   }}
                 >
                   {n}
-                </span>
+                </div>
               )
             })}
             {/* 指針統一由hand定位 */}
             <div className='hand  hour-hand  d-flex flex-row-reverse'
               style={{
                 transform: `rotate(${arr[2]}deg)`,
-                padding:'3px'
+                padding: '3px'
               }}
             >
-             <div className='border border-black'
+              <div className='border border-black'
                 style={{
                   width: '30%',
                 }}
@@ -84,7 +131,7 @@ function App() {
             <div className='hand  min-hand d-flex flex-row-reverse '
               style={{
                 transform: `rotate(${arr[1]}deg)`,
-                padding: '1px'
+                padding: '2px'
               }}
             >
               <div className='border border-white bg-white'
@@ -103,17 +150,9 @@ function App() {
             </div>
             <div className='hand   sec-hand d-flex'
               style={{
-                transform: `rotate(${arr[0]}deg)`
+                transform: `rotate(${arr[0]}deg)`,
               }}
             >
-              {/* <div className=''
-                style={{
-                  width: '100%',
-                  border:'solid',
-                  borderColor:'#88bd26',
-                  borderWidth:'0.5px'
-                }}
-              ></div> */}
 
             </div>
           </div>
